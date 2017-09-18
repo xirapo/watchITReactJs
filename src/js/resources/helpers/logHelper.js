03d8c0b00e22950b0f0e481c8dea7aca75253f03
xirapo <fgarciadiaz29@gmail.com>
2017-09-18 14:42:06 -0600

    Updating bugtracker , repair my DRY

--- src/js/resources/helpers/logHelper.js
+++ src/js/resources/helpers/logHelper.js
@@ -5,38 +5,90 @@
 import Auth from 'resources/database/auth';
 import firebase from 'backend/firebase';
 
+let Logger = ({
+    __init: (user)=> {
+        let db = firebase.database();
+        let dbref = db.ref('user/log/');
+        let auth = new Auth();
 
+        return [auth, dbref]
+    },
+    ok: (data)=> { // Initial sett
+        const [auth,dbref] = Logger.__init();
 
-export default ({
-    ok: (data)=> {
+        //On auth ready
+        auth.authUser.then((user) => {
+            dbref.child(user.uid).push().set({
+                content:data,
+                type:'OK',
+                user:user
+            });
+        });
         console.log('%c' + data, 'color: green;')
     },
-    log: (data)=> {
+    log: (data)=> { // Initial sett
+        const [auth,dbref] = Logger.__init();
+
+        //On auth ready
+        auth.authUser.then((user) => {
+            dbref.child(user.uid).push().set({
+                content:data,
+                type:'LOG',
+                user:user
+            });
+        });
         console.log(data)
     },
     info: (data)=> {
+
+        // Initial sett
+        const [auth,dbref] = Logger.__init();
+
+        //On auth ready
+        auth.authUser.then((user) => {
+            dbref.child(user.uid).push().set({
+                content:data,
+                type:'INFO',
+                user:user
+            });
+        });
         console.info('%c' + data, 'color: blue;')
     },
     warn: (data)=> {
+        // Initial sett
+        const [auth,dbref] = Logger.__init();
+
+        //On auth ready
+        auth.authUser.then((user) => {
+            dbref.child(user.uid).push().set({
+                content:data,
+                type:'WARNNING',
+                user:user
+            });
+        });
+
+
         console.warn('%c' + data, 'color: orange;')
 
     },
     error: (data)=> {
-        let db = firebase.database();
-        let dbref = db.ref('user/log/');
+        //Initial settings
+        const [auth, dbref] = Logger.___init();
 
-        let d = data;
-        let _auth = new Auth();
-
-        _auth.authUser.then((user) => {
-            dbref.push().set({
-                content:d,
+        //On auth ready
+        auth.authUser.then((user) => {
+            dbref.child(user.uid).push().set({
+                content: data,
                 type: 'ERROR',
-                user: user,
+                user: user
             });
         });
 
-        console.error('%c' + data, 'color: red;')
+        //Local log
+        console.error('%c' + data, 'color: red;');
     }
 
-})
+});
+
+//default export
+export default Logger
