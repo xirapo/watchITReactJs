1a0fb70bf00e6c3a5fa544db0f17f28f6fb9b974
gmena <gmjun2000@gmail.com>
2017-09-18 14:53:44 -0600

    -logger - DRY

--- src/js/resources/helpers/logHelper.js
+++ src/js/resources/helpers/logHelper.js
@@ -6,85 +6,63 @@ import Auth from 'resources/database/auth';
 import firebase from 'backend/firebase';
 
 let Logger = ({
+    __setLog: (type, message)=> {
+        // Initial settings
+        const [auth, dbref] = Logger.__init();
+
+        //On auth ready
+        return auth.authUser.then((user) => {
+            dbref.child(user.uid).push().set({
+                content: data,
+                type: type,
+                user: user
+            });
+        });
+    },
     __init: (user)=> {
+        //Initialize database
         let db = firebase.database();
         let dbref = db.ref('user/log/');
         let auth = new Auth();
 
         return [auth, dbref]
     },
-    ok: (data)=> { // Initial sett
-        const [auth,dbref] = Logger.__init();
-
-        //On auth ready
-        auth.authUser.then((user) => {
-            dbref.child(user.uid).push().set({
-                content:data,
-                type:'OK',
-                user:user
-            });
+    ok: (message)=> {
+        //Logging OK
+        Logger.__setLog('OK', message).then(()=> {
+            //local log
+            console.log('%c' + message, 'color: green;');
         });
-    },
-    log: (data)=> { // Initial sett
-        const [auth,dbref] = Logger.__init();
-
-        //On auth ready
-        auth.authUser.then((user) => {
-            dbref.child(user.uid).push().set({
-                content:data,
-                type:'LOG',
-                user:user
-            });
-        });
-        console.log(data)
-    },
-    info: (data)=> {
-
-        // Initial sett
-        const [auth,dbref] = Logger.__init();
-
-        //On auth ready
-        auth.authUser.then((user) => {
-            dbref.child(user.uid).push().set({
-                content:data,
-                type:'INFO',
-                user:user
-            });
-        });
-        console.info('%c' + data, 'color: blue;')
-    },
-    warn: (data)=> {
-        // Initial sett
-        const [auth,dbref] = Logger.__init();
-
-        //On auth ready
-        auth.authUser.then((user) => {
-            dbref.child(user.uid).push().set({
-                content:data,
-                type:'WARNNING',
-                user:user
-            });
-        });
-
-
-        console.warn('%c' + data, 'color: orange;')
 
     },
-    error: (data)=> {
-        //Initial settings
-        const [auth, dbref] = Logger.___init();
-
-        //On auth ready
-        auth.authUser.then((user) => {
-            dbref.child(user.uid).push().set({
-                content: data,
-                type: 'ERROR',
-                user: user
-            });
+    log: (message)=> { // Initial sett
+        //Logging OK
+        Logger.__setLog('LOG', message).then(()=> {
+            console.log(data); //local log
+        });
+    },
+    info: (message)=> {
+        //Logging INFO
+        Logger.__setLog('INFO', message).then(()=> {
+            //local log
+            console.info('%c' + message, 'color: blue;');
         });
 
-        //Local log
-        console.error('%c' + data, 'color: red;');
+    },
+    warn: (message)=> {
+        //Logging INFO
+        Logger.__setLog('WARNING', message).then(()=> {
+            //local log
+            console.warn('%c' + message, 'color: orange;');
+        });
+    },
+    error: (message)=> {
+        //Logging INFO
+        Logger.__setLog('ERROR', message).then(()=> {
+            //local log
+            console.error('%c' + message, 'color: red;');
+        });
+
     }
 
 });
